89eca343af9c16d5c9c23f50f1c2b00439c44264
Ramadhan-Danendra <69399044+Ramadhan-Danendra@users.noreply.github.com>
2026-04-12 22:56:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('Your Personal Japan Itinerary App')
+st.write('*Select your traveling preferences*')
 
-st.write('Hello world!')
+st.write('What kind of places do you prefer?')
+urban = st.checkbox("Urban")
+culture = st.checkbox("Culture")
+nature = st.checkbox("Nature")
+
+st.write(urban, culture, nature)
