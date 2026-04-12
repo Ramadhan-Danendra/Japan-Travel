9c7072d01d3876f850c0e46e63fcc09462c9bcab
Ramadhan-Danendra <69399044+Ramadhan-Danendra@users.noreply.github.com>
2026-04-12 23:06:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,10 @@ st.write('What kind of places do you prefer?')
 urban = st.checkbox("Urban")
 culture = st.checkbox("Culture")
 nature = st.checkbox("Nature")
-
 st.write(urban, culture, nature)
+
+st.write('What season will you problably visit Japan?')
+summer = st.checkbox("Spring")
+summer = st.checkbox("Summer")
+autumn = st.checkbox("Autumn")
+winter = st.checkbox("Winter")
