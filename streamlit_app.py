0577239080b00efb914cbfcf00596c393e05241d
Ramadhan-Danendra <69399044+Ramadhan-Danendra@users.noreply.github.com>
2026-04-13 12:08:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,27 +5,6 @@ st.title('Your Personal Japan Itinerary')
 st.divider()
 st.write('*Select your traveling preferences*')
 
-st.write('What kind of places do you prefer?')
-urban = st.checkbox("Urban")
-culture = st.checkbox("Culture")
-nature = st.checkbox("Nature")
-st.write(urban, culture, nature)
+type = st.multiselect("What kind of places do you prefer?", ["Urban", "Culture", "Nature", "Amusement Park"])
+time = st.multiselect("When will you probably visit Japan?", ["Urban", "Culture", "Nature", "Amusement Park"])
 
-st.write('What season will you probably visit Japan?')
-spring = st.checkbox("Spring")
-summer = st.checkbox("Summer")
-autumn = st.checkbox("Autumn")
-winter = st.checkbox("Winter")
-
-
-A = [spring,summer,autumn,winter]
-for i in A:
-    st.write(i);
-
-type = st.multiselect(
-    "What kind of places do you prefer?",
-    ["Urban", "Culture", "Nature", "Park"])
-
-st.write(type)
-for i in type:
-    st.write(i);
