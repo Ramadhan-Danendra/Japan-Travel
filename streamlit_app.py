239a5b5daea4dbda56d76ab08883a4473678764c
Ramadhan-Danendra <69399044+Ramadhan-Danendra@users.noreply.github.com>
2026-05-02 21:19:49 +0700

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-  page_title = 'Japan Itinerary'
-  page_icon = '🎴'
+  page_title = "Japan Itinerary"
+  page_icon = "🎴"
 )
 
 # Header
