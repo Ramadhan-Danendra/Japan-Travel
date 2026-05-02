00d551b5903def8a01140bb7ffaf94bb5b9d02ab
Ramadhan-Danendra <69399044+Ramadhan-Danendra@users.noreply.github.com>
2026-05-02 21:18:20 +0700

    1

---
+++ pages/1_Preset Itinerary.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+# Header
+st.title('Preset Itinerary')
+st.divider()
+st.write('*Find your personalized itinerary from our custom preset itinerary *')
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+  page_title = 'Japan Itinerary'
+  page_icon = '🎴'
+)
 
 # Header
 st.title('Your Personal Japan Itinerary')
