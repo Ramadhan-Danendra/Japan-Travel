210f698b9d23481eb06c19d7961fbb42274e55a3
Ramadhan-Danendra <69399044+Ramadhan-Danendra@users.noreply.github.com>
2026-05-03 17:12:57 +0700

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -11,11 +11,11 @@ st.title('Your Personal Japan Itinerary')
 st.divider()
 
 
-st.page_link('pages/2_Preset Itinerary.py', label = 'Preset Itinerary')
+st.page_link('pages/2_Preset Itinerary.py', label = 'Preset Itinerary', icon = ':material/alt_route﻿:')
 st.caption('*find your personalized itinerary from our custom preset itinerary*')
 
-st.page_link('pages/3_Custom Itinerary.py', label = 'Custom Itinerary')
+st.page_link('pages/3_Custom Itinerary.py', label = 'Custom Itinerary', icon = ':material/conversion_path:')
 st.caption('*build your own itinerary from cities of your choice*')
 
-st.page_link('pages/4_Generated Itinerary.py', label = 'Generated Itinerary')
+st.page_link('pages/4_Generated Itinerary.py', label = 'Generated Itinerary', icon = ':material/shape_line:')
 st.caption('*Unhappy with our preset itineraries? Unfamiliar with Japan geography? Just select your traveling preferences and let the algorithm generates itinerary for you!*')
