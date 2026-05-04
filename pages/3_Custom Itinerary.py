cd3130ade19f493c6731329a0d804256bc9fdb87
Ramadhan-Danendra <69399044+Ramadhan-Danendra@users.noreply.github.com>
2026-05-04 10:12:57 +0700

    Update 3_Custom Itinerary.py

--- pages/3_Custom Itinerary.py
+++ pages/3_Custom Itinerary.py
@@ -80,7 +80,7 @@ with RC2:
   with st.expander('*see regions map*'):
     st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Regions_and_Prefectures_of_Japan.svg/3840px-Regions_and_Prefectures_of_Japan.svg.png')
 
-st.components.v1.iframe("https://www.google.com/maps/d/embed?mid=138-3NqgV3C79aEhKHId-sPNgskcHT70&ehbc=2E312F&noprof=1")
+st.components.v1.iframe("https://www.google.com/maps/d/embed?mid=138-3NqgV3C79aEhKHId-sPNgskcHT70&ehbc=2E312F&noprof=1", height=1200)
 
 
 # Final
