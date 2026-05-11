4207b0cb35389412ec313f56e3ae8c58989f13d3
Ramadhan-Danendra <69399044+Ramadhan-Danendra@users.noreply.github.com>
2026-05-11 19:36:23 +0700

    Update 3_Custom Itinerary.py

--- pages/3_Custom Itinerary.py
+++ pages/3_Custom Itinerary.py
@@ -50,6 +50,7 @@ if 'Mar' in Month or 'Apr' in Month or 'May' in Month:
   if 'Apr' in Month: cb += ['Early Apr','Mid Apr','Late Apr']
   if 'May' in Month: cb += ['Early May','Mid-Late May']
   CB = st.segmented_control('Do you have the detailed times?', cb, selection_mode="multi")
+  st.caption('Note: cherry blossom time can change every year. The data in this website are based on 2026 cherry blossom time from Japan Meteorological Corporation')
   if 'Early-Mid Mar' in CB or 'Mid-Late May' in CB:
     st.caption('*sorry you cannot see cherry blossom in early-mid March or mid-late May*')
 
@@ -59,6 +60,7 @@ if 'Oct' in Month or 'Nov' in Month or 'Dec' in Month:
   for i in ['Oct','Nov','Dec']:
     if i in Month: ff += ['Early '+i,'Mid '+i,'Late '+i]
   FF = st.segmented_control('Do you have the detailed times?', ff, selection_mode="multi")
+  st.caption('Note: fall foliage time can change every year. The data in this website are based on average fall foliage time from Japan Meteorological Corporation')
   if 'Late Dec' in FF:
     st.caption('*sorry you cannot see fall foliage in late December*')
 
